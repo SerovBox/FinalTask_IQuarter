e972273e5fc85c46122e1109d3ece60d9a7232a9
SerovBox <booxds@gmail.com>
2022-12-27 21:30:36 +0300

    add program

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+using System;
+using static System.Console;
+
+Clear();
+
+string[] array = InputArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n) 
+{
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int n) 
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++)
+    {
+        if(input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] InputArray() 
+{
+    Write("Enter the elements separated by a space: ");
+    return ReadLine().Split(" ");
+}
+*/
